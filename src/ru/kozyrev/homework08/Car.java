2543d387529f9c7dce7f4729bdbdbef22f5fbf49
kozyrev-aa <aa.kozyrev@gmail.com>
2025-09-10 00:07:51 +0500

    + класс Car (Автомобиль)

---
+++ src/ru/kozyrev/homework08/Car.java
@@ -0,0 +1,91 @@
+package ru.kozyrev.homework08;
+
+import java.util.Objects;
+
+public class Car {
+    private String number;
+    private String model;
+    private String color;
+    private double mileage = 0;
+    private double cost;
+
+    public Car(String number, String model, String color, double mileage, double cost) {
+        this.number = number;
+        this.model = model;
+        this.color = color;
+        this.mileage = mileage;
+        this.cost = cost;
+    }
+
+    public Car(String number, String model, String color, double cost) {
+        this.number = number;
+        this.model = model;
+        this.color = color;
+        this.cost = cost;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "number='" + number + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", mileage=" + mileage +
+                ", cost=" + cost +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Double.compare(mileage, car.mileage) == 0 && Double.compare(cost, car.cost) == 0 && Objects.equals(number, car.number) && Objects.equals(model, car.model) && Objects.equals(color, car.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, model, color, mileage, cost);
+    }
+
+}
