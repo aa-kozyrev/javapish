94080aed27d57110266ccd0e200ca5f2fe772ec0
kozyrev-aa <aa.kozyrev@gmail.com>
2025-09-11 00:03:23 +0500

    Задание 4 (Java Stream API)
    Выполнен п.4: "Вывести среднюю стоимость искомой модели modelToFind"

--- src/ru/kozyrev/homework08/Main.java
+++ src/ru/kozyrev/homework08/Main.java
@@ -1,23 +1,46 @@
 package ru.kozyrev.homework08;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        Car auto1 = new Car("ax859kto", "Lada", "Red", 15000, 800000);
-        Car auto2 = new Car("zx365tmn", "KIA", "Green", 150000.8, 1500000.15);
-        Car auto3 = new Car("zx365tmn", "KIA", "Green", 150000.8, 1500000.15);
+        Car auto1 = new Car("a123me", "Mercedes", "White", 8300000);
+        Car auto2 = new Car("b873of", "Volga", "Black", 673000);
+        Car auto3 = new Car("w487mn", "Lexus", "Grey", 76000, 900000);
+        Car auto4 = new Car("p987hj", "Volga", "Red", 610, 704340);
+        Car auto5 = new Car("c987ss", "Toyota", "White", 254000, 761000);
+        Car auto6 = new Car("o983op", "Toyota", "Black", 698000, 740000);
+        Car auto7 = new Car("p146op", "BMW", "White", 271000, 850000);
+        Car auto8 = new Car("u893ii", "Toyota", "Purple", 210900, 440000);
+        Car auto9 = new Car("l097df", "Toyota", "Black", 108000, 780000);
+        Car auto10 = new Car("y876wd", "Toyota", "Black", 160000, 1000000);
 
-        System.out.println(auto1);
-        System.out.println(auto1.hashCode());
+        ArrayList<Car> cars = new ArrayList<>();
+        Collections.addAll(cars, auto1, auto2, auto3, auto4, auto5, auto6, auto7, auto8, auto9, auto10);
 
-        System.out.println(auto2);
-        System.out.println(auto2.hashCode());
-
-        System.out.println(auto1.equals(auto2));
-
+        // 1) Номера всех автомобилей, имеющих заданный в переменной цвет colorToFind или нулевой пробег mileageToFind.
+        System.out.println("::::::: Задание 1 :::::::");
         System.out.println();
-        System.out.println(auto3);
-        System.out.println(auto3.hashCode());
 
-        System.out.println(auto2.equals(auto3));
+        // 2) Количество уникальных моделей в ценовом диапазоне от n до m тыс.
+        System.out.println("::::::: Задание 2 :::::::");
+        System.out.println();
+
+        // 3) Вывести цвет автомобиля с минимальной стоимостью.
+        System.out.println("::::::: Задание 3 :::::::");
+        System.out.println();
+
+        // 4) Среднюю стоимость искомой модели modelToFind
+        System.out.println("::::::: Задание 4 :::::::");
+        final String modelToFind = "Volvo";
+        double avgCost = cars.stream()
+                .filter(car -> car.getModel().equalsIgnoreCase(modelToFind))
+                .mapToDouble(Car::getCost)
+                .average()
+                .orElse(0);
+        System.out.printf("Средняя стоимость модели %s: %.2f", modelToFind, avgCost);
+        System.out.println();
     }
 }
